f72437fbaf8a05f7362c7223b009bf879465f245
Andrew <danweasel@gmail.com>
2015-10-11 00:44:14 +0700

    rewrote BattingYear to take a filename

--- lahman/lahman.go
+++ lahman/lahman.go
@@ -3,7 +3,11 @@ package lahman
 import "errors"
 
 // BattingYear returns all the records from a given year from a list of Batters.
-func BattingYear(year int16, lines []csvReader) ([]*Batter, error) {
+func BattingYear(year int16, file string) ([]*Batter, error) {
+	lines, err := ReadAll(file, Batter{})
+	if err != nil {
+		return nil, err
+	}
 	res := []*Batter{}
 	for _, l := range lines {
 		b, ok := l.(*Batter)
@@ -17,7 +21,11 @@ func BattingYear(year int16, lines []csvReader) ([]*Batter, error) {
 	return res, nil
 }
 
-func PitchingYear(year int16, lines []csvReader) ([]*Pitcher, error) {
+func PitchingYear(year int16, file string) ([]*Pitcher, error) {
+	lines, err := ReadAll(file, Pitcher{})
+	if err != nil {
+		return nil, err
+	}
 	res := []*Pitcher{}
 	for _, l := range lines {
 		p, ok := l.(*Pitcher)
--- lahman/lahman_test.go
+++ lahman/lahman_test.go
@@ -7,16 +7,29 @@ import (
 )
 
 var tail string = "zuninmi01,2014,1,SEA,AL,131,438,51,87,20,2,22,60,0,3,17,158,1,17,0,4,12"
+var pitchtail string = "zimmejo02,2014,1,WAS,NL,14,5,32,32,3,2,0,599,185,59,13,29,182,0.244,2.66,0,4,6,0,800,0,67,5,3,11"
 
 func Test_BattingYear(t *testing.T) {
-	batters, _ := lahman.ReadAll("data/Batting.csv", lahman.Batter{})
-	res, _ := lahman.BattingYear(2014, batters)
+	res, _ := lahman.BattingYear(2014, "data/Batting.csv")
 
 	if len(res) != 1435 {
 		t.Fatal("length of 2014 batters is not 1435")
 	}
 
 	if res[len(res)-1].String() != tail {
+		t.Fatal("Last record does not match tail")
+	}
+}
+
+func Test_PitchingYear(t *testing.T) {
+	res, _ := lahman.PitchingYear(2014, "data/Pitching.csv")
+
+	if len(res) != 746 {
+		t.Log(len(res))
+		t.Fatal("length of 2014 batters is not 1435")
+	}
+
+	if res[len(res)-1].String() != pitchtail {
 		t.Fatal("Last record does not match tail")
 	}
 }
