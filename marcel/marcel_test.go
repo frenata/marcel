e3f9baf8d70ea10fed99f4d4e6f090b4864371ac
Andrew <danweasel@gmail.com>
2015-10-12 21:09:34 +0700

    most DB functionality moved back into lahman, marcel to be reserved for queries

--- marcel/marcel_test.go
+++ marcel/marcel_test.go
@@ -1,12 +1,22 @@
 package marcel_test
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/frenata/marcel/marcel"
+	"github.com/frenata/marcel/lahman"
 )
 
 func Test_GetYear(t *testing.T) {
-	marcel.GetYear(2003)
-
+	count := 0
+	for i := 0; i < 2015; i++ {
+		players := lahman.GetYear(float64(i))
+		for _, p := range players {
+			if p.Pit.SHO() > 0 && p.Pit.H() == 0 {
+				fmt.Println(p)
+				count++
+			}
+		}
+	}
+	fmt.Println("World series shutouts in history: ", count)
 }
