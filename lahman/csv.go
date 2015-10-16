f4371dbdad5c6414581808e5fb7ee2d70bb0e7af
Andrew <danweasel@gmail.com>
2015-10-16 18:30:43 +0700

    partial db read with grep, in progress

--- lahman/csv.go
+++ lahman/csv.go
@@ -3,7 +3,9 @@ package lahman
 import (
 	"bytes"
 	"encoding/csv"
+	"fmt"
 	"io/ioutil"
+	"os/exec"
 	"strconv"
 )
 
@@ -31,38 +33,70 @@ func readAll(file string, c csvReader) ([]csvReader, error) {
 	//fmt.Println(file, c.start(), c.end())
 	//start, end := c.start(), c.end()
 
-	r, err := newReader(file)
-	if err != nil {
-		return nil, err
-	}
-
-	r.Read() // dispose of first line
-	lines, err := r.ReadAll()
-	if err != nil {
-		return nil, err
-	}
-
-	results := make([]csvReader, 0) // len(lines))
-loop:
-	for _, l := range lines {
-		year, err := strconv.Atoi(l[1])
-		if err != nil { //BAttingPost.csv
-			year, err = strconv.Atoi(l[0])
-		}
-		switch {
-		case year < c.start():
-			break
-		case year > c.end() && c.end() != -1:
-			break loop
-		default:
-			b, err := c.csvRead(l)
+	/*
+			r, err := newReader(file)
 			if err != nil {
 				return nil, err
 			}
-			results = append(results, b)
-		}
+
+			r.Read() // dispose of first line
+			lines, err := r.ReadAll()
+			if err != nil {
+				return nil, err
+			}
+
+			results := make([]csvReader, 0) // len(lines))
+			fmt.Println(len(lines))
+		loop:
+			for _, l := range lines {
+				year, err := strconv.Atoi(l[1])
+				if err != nil { //BAttingPost.csv
+					year, err = strconv.Atoi(l[0])
+				}
+				switch {
+				case year < c.start():
+					break
+				case year > c.end() && c.end() != -1:
+					fmt.Println("stop looking")
+					break loop
+				default:
+					b, err := c.csvRead(l)
+					if err != nil {
+						return nil, err
+					}
+					results = append(results, b)
+				}
+			}
+
+			//	fmt.Println(file, len(results))
+			return results, err
+	*/
+
+	years := []int{}
+	for i := c.start(); i < c.end(); i++ {
+		years = append(years, i)
+	}
+	r, err := grepYear(file, years)
+
+}
+
+func grepYear(file string, years []int) (*csv.Reader, error) {
+	var year string
+	for _, y := range years {
+		year = fmt.Sprintf("%s\\|%s", year, strconv.Itoa(y))
+	}
+	year = year[2:]
+
+	cmd := exec.Command("grep", year, file)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
 	}
 
-	//	fmt.Println(file, len(results))
-	return results, err
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	return csv.NewReader(stdout), nil
 }
